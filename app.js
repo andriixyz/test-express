0cf66d21e5d8f019e9252534415b58468c0cf190
Andrii <46864126+andriixyz@users.noreply.github.com>
2023-02-23 13:10:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,59 +1,68 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
+var express = require('express');
+var app = express();
+var fetch = require('node-fetch');
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.listen(3000, function() {
+    console.log("[NodeJS] Application Listening on Port 3000");
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/api/play/:key', function(req, res) {
+    var key = req.params.key;
 
+    var url = 'https://andriixyz.s3.eu-west-1.amazonaws.com/abc3.mp3';
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
+    fetch(url, {
+        method: 'HEAD'
+    }).then(function(response) {
+        if (!response.ok) {
+            return res.sendStatus(404);
+        }
+
+        var stat = {
+            size: parseInt(response.headers.get('content-length'), 10)
+        };
+
+        var range = req.headers.range;
+        var readStream;
+
+        if (range !== undefined) {
+            var parts = range.replace(/bytes=/, "").split("-");
+
+            var partial_start = parts[0];
+            var partial_end = parts[1];
+
+            if ((isNaN(partial_start) && partial_start.length > 1) || (isNaN(partial_end) && partial_end.length > 1)) {
+                return res.sendStatus(500); //ERR_INCOMPLETE_CHUNKED_ENCODING
+            }
+
+            var start = parseInt(partial_start, 10);
+            var end = partial_end ? parseInt(partial_end, 10) : stat.size - 1;
+            var content_length = (end - start) + 1;
+
+            res.status(206).header({
+                'Content-Type': 'audio/mpeg',
+                'Content-Length': content_length,
+                'Content-Range': "bytes " + start + "-" + end + "/" + stat.size
+            });
+
+            console.log({start: start, end: end});
+
+            readStream = fetch(url, {
+                headers: {
+                    Range: 'bytes=' + start + '-' + end
+                }
+            });
+        } else {
+            res.header({
+                'Content-Type': 'audio/mpeg',
+                'Content-Length': stat.size
+            });
+
+            readStream = fetch(url);
+        }
+
+        readStream.then(function(response) {
+            response.body.pipe(res);
         });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+    });
+});
